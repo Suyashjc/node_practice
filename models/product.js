5346484e8d7b532d76b57cec7b4a405d5a185b35
suyashchaudhari <suyash.chaudhari@sts.in>
2020-04-13 12:16:50 +0530

    initial setup

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const db = require('../util/database')
+
+const Cart = require('./cart');
+
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+   return db.execute("insert into products (title, price, discription, imageUrl) values (?, ?, ?, ?)",
+    [this.title, this.price, this.description, this.imageUrl])
+  }
+
+  static deleteById(id) {
+   
+  }
+
+  static fetchAll() {
+   return db.execute('select * from products')
+    
+
+  }
+
+  static findById(id) {
+   return db.execute('select * from products where id = ?',[id])
+  } 
+}
